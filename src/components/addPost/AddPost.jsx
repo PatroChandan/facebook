58a60cb9d9c9a45a522cd8a4940235e81392bfff
PatroChandan <chandanpatro789@gmail.com>
2023-12-06 10:18:08 +0530

    add post changes

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -15,44 +15,27 @@ const AddPost = () => {
   const [title, setTitle] = useState("");
   const [textValue, setTextValue] = useState("");
   const [image, setImage] = useState();
+  const [imageURL, setImageURL] = useState();
 
   const handleImageChange = (e) => {
+    e.preventDefault();
     const file = e.target.files[0];
     console.log("file", file);
-    setImage(URL.createObjectURL(e.target.files[0]));
-    // setImage(e.target.files[0]);
+    setImageURL(URL.createObjectURL(file));
+    setImage(file);
   };
   console.log("path", image);
-  const addPost = async () => {
+  const addPost = async (e) => {
+    e.preventDefault();
     const token = localStorage.getItem("facebook-token");
-    // const posting = await fetch(
-    //   "https://academics.newtonschool.co/api/v1/facebook/post/",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    // "Content-Type":"multipart/form-data"
-    //       Authorization: `Bearer ${token}`,
-    //       projectID: "f104bi07c490",
-    //     },
-    //     body: {
-    //       title: title,
-    //       content: textValue,
-    //       images: image,
-    //     },
-    //   }
-    // );
-
-    // ------------------
     var myHeaders = new Headers();
     myHeaders.append("projectID", "f104bi07c490");
-    myHeaders.append("Content-Type", "multipart/form-data");
-    myHeaders.append("Content-Length:", "2740");
     myHeaders.append("Authorization", "Bearer " + token);
 
     var formdata = new FormData();
     formdata.append("title", "time");
     formdata.append("content", textValue);
-    formdata.append("images", image.name, image);
+    formdata.append("images", image);
 
     var requestOptions = {
       method: "POST",
@@ -60,66 +43,19 @@ const AddPost = () => {
       body: formdata,
     };
 
-    fetch(
+    const resp = await fetch(
       "https://academics.newtonschool.co/api/v1/facebook/post",
       requestOptions
     )
       .then((response) => {
-        console.log("res", response);
         return response;
       })
       .then((result) => {
-        console.log("result", result);
         console.log(result);
       })
       .catch((error) => {
         console.log("error", error);
       });
-    // ---------------------------------------------
-
-    // const formData = new FormData();
-    // formData.append("title", title);
-    // formData.append("content", textValue);
-    // formData.append("images", image);
-
-    // try {
-    //   const response = await axios.post(
-    //     "https://academics.newtonschool.co/api/v1/facebook/post",
-
-    //     {
-    //       headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         projectID: "f104bi07c490",
-    //         "Content-Type": "multipart/form-data",
-    //       },
-    //       body: formData,
-    //     }
-    //   );
-
-    //   console.log("response", response.data);
-
-    //   if (response.data.status === "success") {
-    //     toast.success(response.data.message, {
-    //       position: toast.POSITION.TOP_CENTER,
-    //     });
-    //   } else {
-    //     toast.error(response.data.message, {
-    //       position: toast.POSITION.TOP_CENTER,
-    //     });
-    //   }
-    // } catch (error) {
-    //   console.error("Error adding post:", error);
-    // }
-
-    // if (posting.status === "success") {
-    //   toast.success(posting.message, {
-    //     position: toast.POSITION.TOP_CENTER,
-    //   });
-    // } else {
-    //   toast.error(posting.message, {
-    //     position: toast.POSITION.TOP_CENTER,
-    //   });
-    // }
   };
   return (
     <form className="postForm">
@@ -129,9 +65,16 @@ const AddPost = () => {
           type="text"
           placeholder="What's on your mind ?"
           value={textValue}
-          onChange={(e) => setTextValue(e.target.value)}
+          onChange={(e) => {
+            e.preventDefault();
+            setTextValue(e.target.value);
+          }}
         />
-        <button type="submit" className="btn btn-primary" onClick={addPost}>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={(e) => addPost(e)}
+        >
           Post
         </button>
       </div>
@@ -157,10 +100,7 @@ const AddPost = () => {
       </div>
       {image && (
         <div>
-          <img src={image} alt="Preview" />
-          {/* {uploadPercentage > 0 && (
-            <progress value={uploadPercentage} max="100" />
-          )} */}
+          <img src={imageURL} alt="Preview" />
         </div>
       )}
     </form>
